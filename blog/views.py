fa86fc4865789ce7105672aac0b192198b92d521
isakilikya <isaimadoglu@gmail.com>
2021-04-20 21:45:55 +0300

    django rest framework baslangic

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+
+@api_view(['GET'])
+def getPosts(request):
+    return Response('Blog Posts')
+
+@api_view(['GET'])
+def getPost(request, pk):
+    post = None
+    return Response(post)
