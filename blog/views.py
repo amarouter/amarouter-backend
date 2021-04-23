62ea2a0936ef63672d08155fb770364fb9c6e1be
iremkilikya <iremkilikya@gmail.com>
2021-04-23 19:39:31 +0300

    blog verilerinin mocklanmasi

--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from . import posts
+
 # Create your views here.
 
 @api_view(['GET'])
 def get_posts(request):
-    return Response('Blog Posts')
+    post_list = posts.posts
+    return Response(post_list)
 
 @api_view(['GET'])
 def get_post(request, pk):
-    post = None
-    return Response(post)
+    post = [post for post in posts.posts if str(post['_id']) == pk]
+    if post:
+        return Response(post[0])
+    return Response(None)
